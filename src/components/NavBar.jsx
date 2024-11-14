2169ad6fba2892f7374ebbb7c3dc4df26538e8fa
ShakirMahmud <shakirmahmud50@gmail.com>
2024-11-15 02:30:22 +0600

    Integrate Font, Design navigation and Set up main Layout

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,24 @@
+import { NavLink } from "react-router-dom";
+import userIcon from '../assets/user.png'
+
+
+const NavBar = () => {
+    return (
+        <div className="flex justify-between items-center">
+            <div></div>
+            <div className="nav space-x-5">
+                <NavLink to='/'>Home</NavLink>
+                <NavLink to='/career'>Career</NavLink>
+                <NavLink to='/about'>About</NavLink>
+            </div>
+            <div className="login flex gap-2 items-center">
+                <div className="">
+                    <img src={userIcon} alt="" />
+                </div>
+                    <button className="btn btn-neutral rounded-none">Login</button>
+            </div>
+        </div>
+    );
+};
+
+export default NavBar;
--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,21 +1,24 @@
 import Header from "../components/Header";
 import LatestNews from "../components/LatestNews";
+import NavBar from "../components/NavBar";
 
 
 const HomeLayout = () => {
     return (
-        <div>
+        <div className="font-poppins">
             <header>
                 <Header></Header>
                 <section className="w-11/12 mx-auto">
                     <LatestNews></LatestNews>
                 </section>
             </header>
-            <nav>
-
+            <nav className="w-11/12 mx-auto py-2">
+                <NavBar></NavBar>
             </nav>
-            <main>
-
+            <main className="w-11/12 mx-auto pt-5 grid md:grid-cols-12 gap-3">
+                <aside className="left col-span-3">Left Navbar</aside>
+                <section className="col-span-6">Main Content</section>
+                <aside className="col-span-3">Right Navbar</aside>
             </main>
         </div>
     );
